47d28c3fae1d3625672a8793cb356dde31e0ad6a
kevinlian3 <79417395+kevinlian3@users.noreply.github.com>
2021-05-05 17:16:23 -0700

    major changes

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const tl = gsap.timeline({
     start:"top 65%",
     end:"+=600px",
     pin:true,
+    scrub:true,
     // markers:{
     //   startColor:"purple",
     //   endColor:"purple"
@@ -60,6 +61,9 @@ const tl = gsap.timeline({
 
 tl.to('#frackingExp1',{opacity:0},0);
 tl.to('#step-4',{opacity:1},0);
+tl.to('#step-45 ',{color:"white"},0);
+tl.to('#step-4',{backgroundColor:"black",duration:5},0);
+
 
 
 const video = document.querySelector(".video-background");
@@ -83,6 +87,41 @@ once(document.documentElement, "touchstart", function (e) {
 
 /* ---------------------------------- */
 /* Scroll Control! */
+
+once(video, "loadedmetadata", () => {
+  tl3.fromTo(
+    video,
+    {
+      currentTime: 0
+    },
+    {
+      currentTime: video.duration || 1
+    }
+  );
+});
+
+/* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
+setTimeout(function () {
+  if (window["fetch"]) {
+    fetch(src)
+      .then((response) => response.blob())
+      .then((response) => {
+        var blobURL = URL.createObjectURL(response);
+
+        var t = video.currentTime;
+        once(document.documentElement, "touchstart", function (e) {
+          video.play();
+          video.pause();
+        });
+
+        video.setAttribute("src", blobURL);
+        video.currentTime = t + 0.01;
+      });
+  }
+}, 1000);
+
+/* ---------------------------------- */
+
 
 const tl3 = gsap.timeline({
   defaults: { duration: 1 },
@@ -123,6 +162,7 @@ const tl5 = gsap.timeline({
     //   startColor:"black",
     //   endColor:"black"
     // },
+    toggleActions: "play pause resume reset"
   }
 });
 
@@ -131,6 +171,7 @@ gsap.to("#step-5",{visibility:"visible",opacity:1,
     trigger:".purple",
     start:"top+=200px top",
     // markers:true,
+    toggleActions: "play pause resume reset"
   } 
 });
 
@@ -144,26 +185,110 @@ const tl6 = gsap.timeline({
     //   startColor:"black",
     //   endColor:"black"
     // },
+    toggleActions: "play pause resume reset"
   }
 });
 
 gsap.to("#step-6",{visibility:"visible",opacity:1,
   scrollTrigger:{
     trigger:".purple",
-    start:"top+=800px top",
+    start:"top+=550px top",
     // markers:true,
+    toggleActions: "play pause resume reset"
   } 
 });
 
+const tl7 = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#step-7",
+    start:"top 30%",
+    end:"+=500px",
+    pin:true,
+    // markers:{
+    //   startColor:"black",
+    //   endColor:"black"
+    // },
+    toggleActions: "play pause resume reset"
+  }
+});
 
-once(video, "loadedmetadata", () => {
-  tl3.fromTo(
-    video,
+
+gsap.to("#step-7",{visibility:"visible",opacity:1,
+  scrollTrigger:{
+    trigger:".purple",
+    start:"top+=650px top",
+    // markers:true,
+    toggleActions: "play pause resume reset"
+  } 
+});
+
+/* Horizontal Sexction */ 
+
+let sections = gsap.utils.toArray(".horizpanel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontalScroll",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: () => "+=" + document.querySelector(".horizontalScroll").offsetWidth
+  }
+});
+
+
+/* Pipeline Video */ 
+
+const tl8 = gsap.timeline({
+  defaults: { duration: 1 },
+  scrollTrigger: {
+    trigger: ".panel5",
+    start: "top top",
+    end: "+=2000vh",
+    pin:".panel5",
+    pinSpacing:true,
+    scrub: true,
+    // markers:{
+    //   startColor:"green",
+    //   endColor:"green"
+    // }, 
+  }
+});
+
+
+const video2 = document.querySelector(".video-background2");
+let src2 = video2.currentSrc || video2.src;
+console.log(video2, src2);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+  var onceFn = function (e) {
+    el.removeEventListener(event, onceFn);
+    fn.apply(this, arguments);
+  };
+  el.addEventListener(event, onceFn, opts);
+  return onceFn;
+}
+
+once(document.documentElement, "touchstart", function (e) {
+  video2.play();
+  video2.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+once(video2, "loadedmetadata", () => {
+  tl8.fromTo(
+    video2,
     {
       currentTime: 0
     },
     {
-      currentTime: video.duration || 1
+      currentTime: video2.duration || 1
     }
   );
 });
@@ -188,4 +313,50 @@ setTimeout(function () {
   }
 }, 1000);
 
-/* ---------------------------------- */
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.25, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
+
+
+
+
+
