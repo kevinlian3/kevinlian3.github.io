33700ec62b342aa5c9bbc81a65a48e18a209f6b8
kevinlian3 <79417395+kevinlian3@users.noreply.github.com>
2021-07-06 16:41:22 -0700

    big changes

--- script.js
+++ script.js
@@ -125,7 +125,6 @@ const tl3 = gsap.timeline({
 });
 
 
-
 gsap.to('#frackingVid3',{opacity:1, scrollTrigger: {
     trigger:"#frackingVid3",
     start:"top top",
@@ -209,137 +208,137 @@ gsap.to("#step-7",{visibility:"visible",opacity:1,
 
 /* Horizontal Sexction */ 
 
-let sections = gsap.utils.toArray(".horizpanel");
+// let sections = gsap.utils.toArray(".horizpanel");
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".horizontalScroll",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".horizontalScroll").offsetWidth
-  }
-});
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".horizontalScroll",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: () => "+=" + document.querySelector(".horizontalScroll").offsetWidth
+//   }
+// });
 
 
 /* Pipeline Video */ 
 
-const tl8 = gsap.timeline({
-  defaults: { duration: 1 },
-  scrollTrigger: {
-    trigger: ".panel5",
-    start: "top top",
-    end: "+=2000vh",
-    pin:".panel5",
-    pinSpacing:true,
-    scrub: true,
-    // markers:{
-    //   startColor:"green",
-    //   endColor:"green"
-    // }, 
-  }
-});
+// const tl8 = gsap.timeline({
+//   defaults: { duration: 1 },
+//   scrollTrigger: {
+//     trigger: ".panel5",
+//     start: "top top",
+//     end: "+=2000vh",
+//     pin:".panel5",
+//     pinSpacing:true,
+//     scrub: true,
+//     // markers:{
+//     //   startColor:"green",
+//     //   endColor:"green"
+//     // }, 
+//   }
+// });
 
 
-const video2 = document.querySelector(".video-background2");
-let src2 = video2.currentSrc || video2.src;
-console.log(video2, src2);
+// const video2 = document.querySelector(".video-background2");
+// let src2 = video2.currentSrc || video2.src;
+// console.log(video2, src2);
 
-/* Make sure the video is 'activated' on iOS */
-function once(el, event, fn, opts) {
-  var onceFn = function (e) {
-    el.removeEventListener(event, onceFn);
-    fn.apply(this, arguments);
-  };
-  el.addEventListener(event, onceFn, opts);
-  return onceFn;
-}
+// /* Make sure the video is 'activated' on iOS */
+// function once(el, event, fn, opts) {
+//   var onceFn = function (e) {
+//     el.removeEventListener(event, onceFn);
+//     fn.apply(this, arguments);
+//   };
+//   el.addEventListener(event, onceFn, opts);
+//   return onceFn;
+// }
 
-once(document.documentElement, "touchstart", function (e) {
-  video2.play();
-  video2.pause();
-});
+// once(document.documentElement, "touchstart", function (e) {
+//   video2.play();
+//   video2.pause();
+// });
 
-/* ---------------------------------- */
-/* Scroll Control! */
+// /* ---------------------------------- */
+// /* Scroll Control! */
 
-once(video2, "loadedmetadata", () => {
-  tl8.fromTo(
-    video2,
-    {
-      currentTime: 0
-    },
-    {
-      currentTime: video2.duration || 1
-    }
-  );
-});
+// once(video2, "loadedmetadata", () => {
+//   tl8.fromTo(
+//     video2,
+//     {
+//       currentTime: 0
+//     },
+//     {
+//       currentTime: video2.duration || 1
+//     }
+//   );
+// });
 
-/* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
-setTimeout(function () {
-  if (window["fetch"]) {
-    fetch(src)
-      .then((response) => response.blob())
-      .then((response) => {
-        var blobURL = URL.createObjectURL(response);
+// /* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
+// setTimeout(function () {
+//   if (window["fetch"]) {
+//     fetch(src)
+//       .then((response) => response.blob())
+//       .then((response) => {
+//         var blobURL = URL.createObjectURL(response);
 
-        var t = video.currentTime;
-        once(document.documentElement, "touchstart", function (e) {
-          video.play();
-          video.pause();
-        });
+//         var t = video.currentTime;
+//         once(document.documentElement, "touchstart", function (e) {
+//           video.play();
+//           video.pause();
+//         });
 
-        video.setAttribute("src", blobURL);
-        video.currentTime = t + 0.01;
-      });
-  }
-}, 1000);
+//         video.setAttribute("src", blobURL);
+//         video.currentTime = t + 0.01;
+//       });
+//   }
+// }, 1000);
 
 
-function animateFrom(elem, direction) {
-  direction = direction || 1;
-  var x = 0,
-      y = direction * 100;
-  if(elem.classList.contains("gs_reveal_fromLeft")) {
-    x = -100;
-    y = 0;
-  } else if (elem.classList.contains("gs_reveal_fromRight")) {
-    x = 100;
-    y = 0;
-  }
-  elem.style.transform = "translate(" + x + "px, " + y + "px)";
-  elem.style.opacity = "0";
-  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-    duration: 1.25, 
-    x: 0,
-    y: 0, 
-    autoAlpha: 1, 
-    ease: "expo", 
-    overwrite: "auto"
-  });
-}
+// function animateFrom(elem, direction) {
+//   direction = direction || 1;
+//   var x = 0,
+//       y = direction * 100;
+//   if(elem.classList.contains("gs_reveal_fromLeft")) {
+//     x = -100;
+//     y = 0;
+//   } else if (elem.classList.contains("gs_reveal_fromRight")) {
+//     x = 100;
+//     y = 0;
+//   }
+//   elem.style.transform = "translate(" + x + "px, " + y + "px)";
+//   elem.style.opacity = "0";
+//   gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+//     duration: 1.25, 
+//     x: 0,
+//     y: 0, 
+//     autoAlpha: 1, 
+//     ease: "expo", 
+//     overwrite: "auto"
+//   });
+// }
 
-function hide(elem) {
-  gsap.set(elem, {autoAlpha: 0});
-}
+// function hide(elem) {
+//   gsap.set(elem, {autoAlpha: 0});
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-  gsap.registerPlugin(ScrollTrigger);
+// document.addEventListener("DOMContentLoaded", function() {
+//   gsap.registerPlugin(ScrollTrigger);
   
-  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
-    hide(elem); // assure that the element is hidden when scrolled into view
+//   gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+//     hide(elem); // assure that the element is hidden when scrolled into view
     
-    ScrollTrigger.create({
-      trigger: elem,
-      onEnter: function() { animateFrom(elem) }, 
-      onEnterBack: function() { animateFrom(elem, -1) },
-      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-    });
-  });
-});
+//     ScrollTrigger.create({
+//       trigger: elem,
+//       onEnter: function() { animateFrom(elem) }, 
+//       onEnterBack: function() { animateFrom(elem, -1) },
+//       onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+//     });
+//   });
+// });
 
 
 
